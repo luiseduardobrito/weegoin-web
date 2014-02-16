f6b1f5048d8816b84b26d63b0e865e8607721441
Luís Eduardo Brito <luiseduardo14@gmail.com>
2014-02-16 18:06:07 -0300

    express server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var server = express();
+server.configure(function(){
+	server.use(express.static(__dirname + '/public'));
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port);
